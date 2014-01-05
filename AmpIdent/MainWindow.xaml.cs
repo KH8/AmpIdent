f74bbe611ec8e0f1f9e097c813c9a7ba3872a200
H8 <reczek.krzysztof@gmail.com>
2014-01-05 17:08:32 +0100

    oxyPlot testing

--- AmpIdent/MainWindow.xaml.cs
+++ AmpIdent/MainWindow.xaml.cs
@@ -24,116 +24,18 @@ namespace AmpIdent
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ViewModels.MainWindowModel viewModel;
+        public PlotModel MyPlotModel
+        {
+            get { return viewModel.MyPlotModel; }
+        }
+
         public MainWindow()
         {
-            InitializeComponent();
-            string newLine = Environment.NewLine;
-
-            /*
-            //first Czech example
-            // parameters
+            viewModel = new ViewModels.MainWindowModel();
+            DataContext = viewModel;
+ 
             
-            double k = 0.2; 
-            double q = 1;
-            double sigma2 = 0.5;
-
-            Random random = new Random();
-            
-            // experiments
-
-            var X = new DenseMatrix(10, 1, 0);
-            var Y = new DenseMatrix(10, 1, 0);
-
-            for (int i = 0; i <= 9; i++)
-            {
-                X[i, 0] = i;
-                Y[i, 0] = k * X[i, 0] + q + sigma2 * random.Next(0, 10);
-            }
-
-            Console.WriteLine(newLine + "Vector X:");
-            Console.WriteLine(X.ToString());
-            Console.WriteLine(newLine + "Vector Y:");
-            Console.WriteLine(Y.ToString());
-            ///*/
-
-            /*
-            //second exapmle
-            var vectorX = new DenseMatrix(10,1,1);
-            var vectorY = new DenseMatrix(10, 1, 1);
-
-            for (int i = 0; i <= 9; i++)
-            {
-                vectorX[i,0] = i;
-            }
-            Console.WriteLine(newLine + "Vector X:");
-            Console.WriteLine(vectorX.ToString());
-
-            for (int i = 0; i <= 9; i++)
-            {
-                vectorY[i,0] = 1.234F * vectorX[i,0] + 4.523F;
-            }
-            Console.WriteLine(newLine + "Vector Y:");
-            Console.WriteLine(vectorY.ToString());
-
-            var matrixZ = new DenseMatrix(4, 4, 1);
-
-            int k = 5;
-
-            matrixZ[0, 0] = vectorY[k - 1,0];
-            matrixZ[0, 1] = vectorY[k - 2,0];
-            matrixZ[0, 2] = vectorX[k - 1,0];
-            matrixZ[0, 3] = vectorX[k - 2,0];
-
-            matrixZ[1, 0] = vectorY[k,0];
-            matrixZ[1, 1] = vectorY[k - 1,0];
-            matrixZ[1, 2] = vectorX[k,0];
-            matrixZ[1, 3] = vectorX[k - 1,0];
-            
-            matrixZ[2, 0] = vectorY[k + 1,0];
-            matrixZ[2, 1] = vectorY[k,0];
-            matrixZ[2, 2] = vectorX[k + 1,0];
-            matrixZ[2, 3] = vectorX[k,0];
-            
-            matrixZ[3, 0] = vectorY[k + 1,0];
-            matrixZ[3, 1] = vectorY[k + 2,0];
-            matrixZ[3, 2] = vectorX[k + 1,0];
-            matrixZ[3, 3] = vectorX[k + 2,0];
-
-            var vectorYk = new DenseMatrix(4, 1, 1);
-            vectorYk[0, 0] = vectorY[k, 0];
-            vectorYk[1, 0] = vectorY[k+1, 0];
-            vectorYk[2, 0] = vectorY[k+2, 0];
-            vectorYk[3, 0] = vectorY[k+3, 0];
-
-            Console.WriteLine(newLine + "Matrix Z:");
-            Console.WriteLine(matrixZ.ToString());
-
-            var matrixZT = matrixZ.Transpose();
-            Console.WriteLine(newLine + "Matrix ZT:");
-            Console.WriteLine(matrixZT.ToString());
-
-            var matrixZTZ = matrixZT*matrixZ;
-            var matrixTheta = matrixZTZ.Inverse() * matrixZT * vectorYk;
-            Console.WriteLine(newLine + "Matrix Theta:");
-            Console.WriteLine(matrixTheta.ToString());
-
-            var tempX = new DenseMatrix(20, 1, 1);
-            var tempY = new DenseMatrix(20, 1, 1);
-
-            for (int i = 0; i <= 19; i++)
-            {
-                tempX[i, 0] = i;
-            }
-
-            for (int i = 2; i <= 19; i++)
-            {
-                tempY[i, 0] = -1 * matrixTheta[0, 0] * tempY[i - 1, 0] - matrixTheta[1, 0] * tempY[i - 2, 0] + matrixTheta[2, 0] * tempX[i - 1, 0] + matrixTheta[3, 0] * tempX[i - 2, 0];
-            }
-
-            Console.WriteLine(newLine + "Solved:");
-            Console.WriteLine(tempX.ToString());
-            Console.WriteLine(tempY.ToString());
-            ///*/
         }
     }
 }
---
+++ AmpIdent/ViewModel/MainWindowModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using OxyPlot.Annotations;
+using OxyPlot.Reporting;
+
+namespace AmpIdent.ViewModels
+{
+    public class MainWindowModel : INotifyPropertyChanged
+    {
+        private PlotModel plotModel;
+        public PlotModel MyPlotModel
+        {
+            get { return plotModel; }
+            set { plotModel = value; OnPropertyChanged("MyPlotModel"); }
+        }
+
+        public MainWindowModel()
+        {
+            var temp = new PlotModel("Square wave");
+            var ls = new LineSeries("sin(x)+sin(3x)/3+sin(5x)/5+...");
+            int n = 10;
+            for (double x = -10; x < 10; x += 0.0001)
+            {
+                double y = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    int j = i * 2 + 1;
+                    y += Math.Sin(j * x) / j;
+                }
+                ls.Points.Add(new DataPoint(x, y));
+            }
+            temp.Series.Add(ls);
+            temp.Axes.Add(new LinearAxis(AxisPosition.Left, -4, 4));
+            temp.Axes.Add(new LinearAxis(AxisPosition.Bottom));
+            plotModel = temp; 
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
