f41980032d1a706cb698acd1b4dc5937ae575281
H8 <reczek.krzysztof@gmail.com>
2014-01-05 18:48:26 +0100

    Least Squeres simple example

--- AmpIdent/MainWindow.xaml.cs
+++ AmpIdent/MainWindow.xaml.cs
@@ -31,29 +31,60 @@ namespace AmpIdent
             MainViewModel mainViewModel = new MainViewModel();
             string newLine = Environment.NewLine;
 
-            ///*
+            
             //first Czech example
             // parameters
             
             double k = 0.2; 
             double q = 1;
-            double sigma2 = 0.5;
+            double sigma2 = 5;
 
             Random random = new Random();
             
             // experiments
 
-            var X = new DenseMatrix(10, 1, 0);
-            var Y = new DenseMatrix(10, 1, 0);
+            var X = new DenseMatrix(10, 1, 0.0);
+            var Y = new DenseMatrix(10, 1, 0.0);
 
-            for (int i = 0; i <= 9; i++)
+            for (int j = 1; j <= 10; j++)
             {
-                X[i, 0] = i;
-                Y[i, 0] = k * X[i, 0] + q + sigma2 * random.Next(0, 10);
+                int i = j - 1;
+                X[i, 0] = j;
+                Y[i, 0] = k * X[i, 0] + q + random.Next(-5,5)/10.0;
                 mainViewModel.AddPoint(1, new DataPoint(X[i, 0], Y[i, 0]));
                 mainViewModel.AddPoint(2, new DataPoint(X[i, 0], k * X[i, 0] + q));
             }
-            ///*/
+            Console.WriteLine(X.ToString());
+            Console.WriteLine(Y.ToString());
+
+            var Z = new DenseMatrix(10, 2, 1.0);
+            for (int i = 0; i <= 9; i++)
+            {
+                Z[i, 0] = X[i, 0];
+            }
+            Console.WriteLine(Z.ToString());
+
+            var ZT = Z.Transpose();
+            Console.WriteLine(ZT.ToString());
+
+            var ZTZ = ZT * Z;
+            Console.WriteLine(ZTZ.ToString());
+
+            var determinant = ZTZ.Determinant();
+            Console.WriteLine(newLine + "Determinant: " +determinant.ToString() +newLine);
+
+            var Theta = ZTZ.Inverse() * ZT * Y;
+            Console.WriteLine(Theta.ToString());
+
+            for (int j = 1; j <= 10; j++)
+            {
+                int i = j - 1;
+                X[i, 0] = j;
+                Y[i, 0] = Theta[0, 0] * X[i, 0] + Theta[1, 0];
+                mainViewModel.AddPoint(3, new DataPoint(X[i, 0], Y[i, 0]));
+            }
+            Console.WriteLine(X.ToString());
+            Console.WriteLine(Y.ToString());
 
             DataContext = mainViewModel;
         }
