f056536a19bff81336e44b13357df8757ddebf26
H8 <reczek.krzysztof@gmail.com>
2014-01-06 00:20:43 +0100

    Aproximation for random input signal | Resources Directory added

--- AmpIdent/MainWindow.xaml.cs
+++ AmpIdent/MainWindow.xaml.cs
@@ -40,7 +40,7 @@ namespace AmpIdent
             double k3 = -3;
             double k4 = -0.9;
             double q = -4.53;
-            int sigma2 = 9;
+            int sigma2 = 9000;
 
             Random random = new Random();
             
@@ -52,23 +52,22 @@ namespace AmpIdent
             for (int j = 1; j <= 500; j++)
             {
                 int i = j - 1;
-                X1[i, 0] = j;
-                Y1[i, 0] = /*k4 * Math.Pow(X[i, 0] *  k3 * Math.Pow(X[i, 0]+9, 2),9) + k2 * Math.Pow(X[i, 0], 2) +*/ k1 * X1[i, 0] +  q; /* * random.Next(-6 * sigma2, 6* sigma2); */
+
+                if (i != 0) X1[i, 0] = random.Next((int)X1[i - 1, 0], (int)X1[i - 1, 0] + 10);
+                else X1[i, 0] = random.Next(0, 10);
+
+                Y1[i, 0] = k4 * Math.Pow(X1[i, 0], -2) *  k3 * Math.Pow(X1[i, 0], -3) + k2 * Math.Pow(X1[i, 0], 2) + k1 * X1[i, 0] +  q + random.Next(-6 * sigma2, 6* sigma2);
                 mainViewModel.AddPoint(1, new DataPoint(X1[i, 0], Y1[i, 0]));
-                mainViewModel.AddPoint(2, new DataPoint(X1[i, 0], k1 * X1[i, 0] + q));
+                mainViewModel.AddPoint(2, new DataPoint(X1[i, 0], k4 * Math.Pow(X1[i, 0], -2) * k3 * Math.Pow(X1[i, 0], -3) + k2 * Math.Pow(X1[i, 0], 2) + k1 * X1[i, 0] + q));
             }
 
-            var Z = new DenseMatrix(500, 8, 1.0);
-            for (int i = 10; i <= 498; i++)
+            var Z = new DenseMatrix(500, 4, 1.0);
+            for (int i = 0; i <= 498; i++)
             {
-                Z[i, 0] = -1 * Y1[i - 1, 0];
-                Z[i, 1] = -1 * Y1[i - 2, 0];
-                Z[i, 2] = -1 * Y1[i - 3, 0];
-                Z[i, 3] = 1;
-                Z[i, 4] = X1[i, 0];
-                Z[i, 5] = X1[i - 1, 0];
-                Z[i, 6] = X1[i - 2, 0];
-                Z[i, 7] = X1[i - 3, 0];
+                Z[i, 0] = 1;
+                Z[i, 1] = X1[i, 0];
+                Z[i, 2] = X1[i, 0] * X1[i, 0];
+                Z[i, 3] = X1[i, 0] * X1[i, 0] * X1[i, 0];
             }
             Console.WriteLine(Z.ToString());
 
@@ -87,18 +86,10 @@ namespace AmpIdent
             var X = new DenseMatrix(500, 1, 0.0);
             var Y = new DenseMatrix(500, 1, 0.0);
 
-            for (int i = 0; i <= 11; i++)
+            for (int i = 0; i <= 499; i++)
             {
-                X[i, 0] = X1[i, 0];
-                Y[i, 0] = Y1[i, 0];
-            }
-
-            for (int j = 11; j <= 500; j++)
-            {
-                int i = j - 1;
-                X[i, 0] = j;
-                Y[i, 0] = -1 * Theta[0, 0] * Y[i - 1, 0] - Theta[1, 0] * Y[i - 2, 0] - Theta[2, 0] * Y[i - 3, 0] + Theta[3, 0] + Theta[4, 0] * Y[i, 0] + Theta[5, 0] * Y[i - 1, 0] + Theta[6, 0] * Y[i - 2, 0] + Theta[7, 0] * Y[i - 3, 0];
-                mainViewModel.AddPoint(9, new DataPoint(X[i, 0], Y[i, 0]));
+                Y[i, 0] = Theta[0, 0] + Theta[1, 0] * X1[i, 0] + Theta[2, 0] * X1[i, 0] * X1[i, 0] + Theta[3, 0] * X1[i, 0] * X1[i, 0] * X1[i, 0];
+                mainViewModel.AddPoint(9, new DataPoint(X1[i, 0], Y[i, 0]));
             }
             
             DataContext = mainViewModel;
