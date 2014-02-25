fda29fb22b5d624007072702dc0f81e67f4bd203
KH8 <reczek.krzysztof@gmail.com>
2014-02-25 10:48:43 +0100

    GPU kernel ...

--- AmpIdent/AmpIdentGPUkernel/AmpIdentGPUkernel/Debug/kernel.cudafe2.c
+++
@@ -1,7 +0,0 @@
-#line 1 "Debug/kernel.cudafe1.gpu"
-#line 430 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\crtdefs.h"
-typedef unsigned size_t;
-#include "crt/host_runtime.h"
-void *memcpy(void*, const void*, size_t); void *memset(void*, int, size_t);
-
-#include "Debug/kernel.cudafe2.stub.c"
