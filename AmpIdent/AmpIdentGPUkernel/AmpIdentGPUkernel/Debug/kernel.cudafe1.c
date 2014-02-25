bd78d7710c14f88fa9ec1894791287c732cc9747
KH8 <reczek.krzysztof@gmail.com>
2014-02-25 10:39:38 +0100

    GPU kernel ...

---
+++ AmpIdent/AmpIdentGPUkernel/AmpIdentGPUkernel/Debug/kernel.cudafe1.c
@@ -0,0 +1,228 @@
+#line 1 "C:/Users/H8/Documents/Visual Studio 2012/Projects/AmpIdent/AmpIdentGPUkernel/AmpIdentGPUkernel/kernel.cu"
+#line 702 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\sal.h"
+#pragma region Input Buffer SAL 1 compatibility macros
+#line 1466 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\sal.h"
+#pragma endregion Input Buffer SAL 1 compatibility macros
+#line 179 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\string.h"
+#pragma warning(push)
+#pragma warning(disable:6059)
+
+
+#pragma warning(pop)
+#line 308 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\string.h"
+#pragma warning(push)
+#pragma warning(disable:6059)
+
+#pragma warning(pop)
+#line 27 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\wtime.inl"
+#pragma warning(push)
+#pragma warning(disable:4996)
+#line 58 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\wtime.inl"
+#pragma warning(pop)
+#line 185 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\yvals.h"
+#pragma detect_mismatch("_MSC_VER", "1700")
+#line 189 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\yvals.h"
+#pragma detect_mismatch("_ITERATOR_DEBUG_LEVEL", "2")
+#line 200 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\yvals.h"
+#pragma detect_mismatch("RuntimeLibrary", "MDd_DynamicDebug")
+#line 27 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\use_ansi.h"
+#pragma comment(lib, "msvcprtd")
+#line 9 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\xtr1common"
+#pragma warning(push,3)
+#line 417 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\xtr1common"
+#pragma warning(pop)
+#line 804 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdlib.h"
+#pragma warning (push)
+#pragma warning (disable:6540)
+#line 816 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdlib.h"
+#pragma warning (pop)
+#line 904 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdlib.h"
+#pragma warning(push)
+#pragma warning(disable: 4141)
+#line 918 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdlib.h"
+#pragma warning(pop)
+#line 21 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\xstddef"
+#pragma warning(push,3)
+#line 1126 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\xstddef"
+#pragma warning(pop)
+#line 10 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\exception"
+#pragma warning(push,3)
+#line 199 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\malloc.h"
+#pragma warning(push)
+#pragma warning(disable:6540)
+#line 210 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\malloc.h"
+#pragma warning(pop)
+#line 237 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\malloc.h"
+#pragma warning(push)
+#pragma warning(disable: 6014)
+#line 258 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\malloc.h"
+#pragma warning(pop)
+#line 562 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\exception"
+#pragma warning(pop)
+#line 9 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\new"
+#pragma warning(push,3)
+#line 53 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\new"
+#pragma warning (suppress: 4985)
+#line 106 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\new"
+#pragma warning(pop)
+#line 11 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\xtgmath.h"
+#pragma warning(push,3)
+#line 112 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\xtgmath.h"
+#pragma warning(pop)
+#line 239 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdio.h"
+#pragma warning(push)
+#pragma warning(disable:6530)
+#line 245 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdio.h"
+#pragma warning(pop)
+#line 292 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdio.h"
+#pragma warning(push)
+#pragma warning(disable:6530)
+#line 298 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdio.h"
+#pragma warning(pop)
+#line 305 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdio.h"
+#pragma warning(push)
+#line 305 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdio.h"
+#pragma warning(disable: 4793)
+#line 305 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdio.h"
+#pragma warning(pop)
+#line 309 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdio.h"
+#pragma warning(push)
+#line 309 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdio.h"
+#pragma warning(disable: 4793)
+#line 309 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdio.h"
+#pragma warning(pop)
+
+
+
+#pragma warning(push)
+#pragma warning(disable:6530)
+#line 323 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdio.h"
+#pragma warning(pop)
+#line 347 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdio.h"
+#pragma warning(push)
+#pragma warning(disable:4793)
+
+#pragma warning(pop)
+#line 355 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdio.h"
+#pragma warning(push)
+#pragma warning(disable:4793)
+
+#pragma warning(pop)
+#line 459 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdio.h"
+#pragma warning(push)
+#line 459 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdio.h"
+#pragma warning(disable: 4793)
+#line 459 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdio.h"
+#pragma warning(pop)
+#line 469 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdio.h"
+#pragma warning(push)
+#line 469 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdio.h"
+#pragma warning(disable: 4793)
+#line 469 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdio.h"
+#pragma warning(pop)
+
+
+#pragma warning(push)
+#pragma warning(disable:4793)
+
+#pragma warning(pop)
+#line 528 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdio.h"
+#pragma warning(push)
+#pragma warning(disable:4141 4996 4793)
+
+
+#pragma warning(pop)
+#line 34 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\swprintf.inl"
+#pragma warning( push )
+#pragma warning( disable : 4793 4412 )
+#line 45 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\swprintf.inl"
+#pragma warning( pop )
+
+#pragma warning( push )
+#pragma warning( disable : 4412 )
+
+
+
+
+#pragma warning( pop )
+#line 58 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\swprintf.inl"
+#pragma warning( push )
+#pragma warning( disable : 4793 4412 )
+#line 69 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\swprintf.inl"
+#pragma warning( pop )
+
+#pragma warning( push )
+#pragma warning( disable : 4412 )
+
+
+
+
+#pragma warning( pop )
+
+
+#pragma warning( push )
+#pragma warning( disable : 4996 )
+
+#pragma warning( push )
+#pragma warning( disable : 4793 4141 )
+#line 93 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\swprintf.inl"
+#pragma warning( pop )
+
+#pragma warning( push )
+#pragma warning( disable : 4141 )
+
+
+
+
+#pragma warning( pop )
+
+#pragma warning( push )
+#pragma warning( disable : 4793 4141 )
+#line 113 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\swprintf.inl"
+#pragma warning( pop )
+
+#pragma warning( push )
+#pragma warning( disable : 4141 )
+
+
+
+
+#pragma warning( pop )
+
+#pragma warning( pop )
+#line 562 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdio.h"
+#pragma warning(push)
+#pragma warning(disable:6530)
+#line 584 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\stdio.h"
+#pragma warning(pop)
+#line 550 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\yvals.h"
+#pragma warning(push)
+#pragma warning(disable:4412)
+#line 728 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\yvals.h"
+#pragma warning(pop)
+#line 543 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\yvals.h"
+enum _ZSt14_Uninitialized {
+
+_ZSt7_Noinit};
+#line 430 "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\include\\crtdefs.h"
+typedef unsigned size_t;
+#include "crt/host_runtime.h"
+void *memcpy(void*, const void*, size_t); void *memset(void*, int, size_t);
+#line 9 "C:/Users/H8/Documents/Visual Studio 2012/Projects/AmpIdent/AmpIdentGPUkernel/AmpIdentGPUkernel/kernel.cu"
+extern int main(void);
+extern int __cudaSetupArgSimple();
+extern int __cudaLaunch();
+extern void __nv_dummy_param_ref();
+extern int __cudaRegisterEntry();
+extern int __cudaRegisterBinary();
+static void __sti___14_kernel_cpp1_ii_b614c5e6(void);
+#pragma section(".CRT$XCU",read,write)
+__declspec(allocate(".CRT$XCU"))static void (*__dummy_static_init__sti___14_kernel_cpp1_ii_b614c5e6[])(void) = {__sti___14_kernel_cpp1_ii_b614c5e6};
+#line 9 "C:/Users/H8/Documents/Visual Studio 2012/Projects/AmpIdent/AmpIdentGPUkernel/AmpIdentGPUkernel/kernel.cu"
+int main(void)
+{
+return 0;
+}
+static void __sti___14_kernel_cpp1_ii_b614c5e6(void) {   }
+
+#include "Debug/kernel.cudafe1.stub.c"
