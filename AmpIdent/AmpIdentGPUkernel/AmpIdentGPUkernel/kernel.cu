fb75ec30191eca8842b46075e227e10edfbf3196
KH8 <reczek.krzysztof@gmail.com>
2014-02-26 20:11:09 +0100

    CUDA parallel example

--- AmpIdent/AmpIdentGPUkernel/AmpIdentGPUkernel/kernel.cu
+++ AmpIdent/AmpIdentGPUkernel/AmpIdentGPUkernel/kernel.cu
@@ -1,12 +1,60 @@
-#include "cuda_runtime.h"
+#include <cuda_runtime.h>
 #include <stdio.h>
+#include <device_launch_parameters.h>
  
-__global__ void kernel(int a, int b, int *c)
+__global__ void kernel(int *m, int v)
 {
-    *c = (a + b)*(a + b);
+     m[threadIdx.x + (blockDim.x * blockIdx.x)] = v;
+}
+ 
+// Print device properties
+void printDevProp(cudaDeviceProp devProp)
+{
+    printf("Major revision number:         %d\n",  devProp.major);
+    printf("Minor revision number:         %d\n",  devProp.minor);
+    printf("Name:                          %s\n",  devProp.name);
+    printf("Total global memory:           %u\n",  devProp.totalGlobalMem);
+    printf("Total shared memory per block: %u\n",  devProp.sharedMemPerBlock);
+    printf("Total registers per block:     %d\n",  devProp.regsPerBlock);
+    printf("Warp size:                     %d\n",  devProp.warpSize);
+    printf("Maximum memory pitch:          %u\n",  devProp.memPitch);
+    printf("Maximum threads per block:     %d\n",  devProp.maxThreadsPerBlock);
+    for (int i = 1; i <= 3; ++i)
+    printf("Maximum dimension %d of block:  %d\n", i, devProp.maxThreadsDim[i-1]);
+    for (int i = 1; i <= 3; ++i)
+    printf("Maximum dimension %d of grid:   %d\n", i, devProp.maxGridSize[i-1]);
+    printf("Clock rate:                    %d\n",  devProp.clockRate);
+    printf("Total constant memory:         %u\n",  devProp.totalConstMem);
+    printf("Texture alignment:             %u\n",  devProp.textureAlignment);
+    printf("Concurrent copy and execution: %s\n",  (devProp.deviceOverlap ? "Yes" : "No"));
+    printf("Number of multiprocessors:     %d\n",  devProp.multiProcessorCount);
+    printf("Kernel execution timeout:      %s\n",  (devProp.kernelExecTimeoutEnabled ? "Yes" : "No"));
+    return;
 }
  
 int main()
 {
+	/*
+    // Number of CUDA devices
+    int devCount;
+    cudaGetDeviceCount(&devCount);
+    printf("CUDA Device Query...\n");
+    printf("There are %d CUDA devices.\n", devCount);
+ 
+    // Iterate through devices
+    for (int i = 0; i < devCount; ++i)
+    {
+        // Get device properties
+        printf("\nCUDA Device #%d\n", i);
+        cudaDeviceProp devProp;
+        cudaGetDeviceProperties(&devProp, i);
+        printDevProp(devProp);
+    }
+ 
+    printf("\nPress any key to exit...");
+    char c;
+    scanf("%c", &c);
+	//*/
+
     return 0;
 }
