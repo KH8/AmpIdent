70447753e432ac6340f00ac9c352c234674f7a8a
KH8 <reczek.krzysztof@gmail.com>
2014-02-28 22:33:03 +0100

    CUDA optimization ...

--- AmpIdent/Estimation/MultiplicatorCuda.cs
+++ AmpIdent/Estimation/MultiplicatorCuda.cs
@@ -2,6 +2,7 @@ using System;
 using System.Security.Cryptography;
 using ManagedCuda;
 using ManagedCuda.BasicTypes;
+using ManagedCuda.VectorTypes;
 using MathNet.Numerics.LinearAlgebra.Double;
 
 namespace AmpIdent.Estimation
@@ -12,38 +13,51 @@ namespace AmpIdent.Estimation
         private static int _threadsPerBlock;
         static CudaKernel _multiplyTwoVectorWithCuda;
         private static CudaContext _cnContext;
-        private static Boolean _initialized;
 
         static void InitKernels()
         {
             _matrixSize = 256;
-            _threadsPerBlock = 256;
+            _threadsPerBlock = 512;
 
-            _cnContext = new CudaContext();
+            _cnContext = new CudaContext(CudaContext.GetMaxGflopsDeviceId());
             CUmodule cumodule = _cnContext.LoadModule(@"\Kernel\kernel.ptx");
             _multiplyTwoVectorWithCuda = new CudaKernel("_Z6kernel_", cumodule, _cnContext);
+        }
 
-            _initialized = true;
+        static void CleanUpResources()
+        {
+            if (_cnContext != null) _cnContext.Dispose();
         }
 
         public static Func<DenseMatrix, DenseMatrix, DenseMatrix> Multiply = (m1, m2) =>
         {
             // init parameters
-            if (!_initialized) InitKernels();
+            InitKernels();
 
             _matrixSize = m1.RowCount * m2.ColumnCount;
             _multiplyTwoVectorWithCuda.BlockDimensions = _threadsPerBlock;
             _multiplyTwoVectorWithCuda.GridDimensions = m1.RowCount / _threadsPerBlock + 1;
 
-            CudaDeviceVariable<float> matrixM1 = MatrixConverter.DenseMatrix2Floats(m1);
-            CudaDeviceVariable<float> matrixM2 = MatrixConverter.DenseMatrix2Floats(m2);
+            var matrixM1Float = MatrixConverter.DenseMatrix2Floats(m1);
+            CudaDeviceVariable<float> matrixM1 = matrixM1Float;
+            matrixM1.CopyToDevice(matrixM1Float);
+            var matrixM2Float =MatrixConverter.DenseMatrix2Floats(m2);
+            CudaDeviceVariable<float> matrixM2 = matrixM2Float;
+            matrixM2.CopyToDevice(matrixM2Float);
             var matrixM = new CudaDeviceVariable<float>(_matrixSize);
+
             // run cuda method
             _multiplyTwoVectorWithCuda.Run(matrixM1.DevicePointer, matrixM2.DevicePointer, matrixM.DevicePointer,
                 m1.ColumnCount, m2.ColumnCount, m1.RowCount, m2.RowCount);
+
             // copy return to host
             var output = new float[_matrixSize];
             matrixM.CopyToHost(output);
+
+            matrixM1.Dispose();
+            matrixM2.Dispose();
+            matrixM.Dispose();
+            CleanUpResources();
 
             return MatrixConverter.Floats2DenseMatrix(output, m2.ColumnCount, m1.RowCount);
         };
